f27aaa796c109bbddd5cc7cc28201f5cd3a90735
James Lao <git@jameslao.com>
2019-02-20 01:06:42 -0800

    Refactor into CacheTableInternal reusable struct.
    This refactoring allows us to reuse most of the code to build a concurrent version of CacheTable.

--- CacheTable/CacheTable.cs
+++ CacheTable/CacheTable.cs
@@ -1,24 +1,20 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 
 namespace CacheTable
 {
     public class CacheTable<TKey, TValue> : IEnumerable<KeyValuePair<TKey, TValue>>
     {
-        private readonly KeyValuePair<TKey, TValue>?[] table;
-        private readonly int numRows;
-        private readonly int numColumns;
+        private CacheTableInternal<TKey, TValue> table;
         private readonly Random rng = new Random();
-        private int count;
 
         public CacheTable(int rows, int columns)
         {
-            this.table = new KeyValuePair<TKey, TValue>?[rows * columns];
-            this.numRows = rows;
-            this.numColumns = columns;
+            this.table = new CacheTableInternal<TKey, TValue>(rows, columns);
         }
+
+        public int Count => this.table.count;
 
         public TValue this[TKey key]
         {
@@ -35,120 +31,41 @@ namespace CacheTable
 
             set
             {
-                var kvp = new KeyValuePair<TKey, TValue>(key, value);
-                (int start, int end) = this.FindRow(key);
-                int empty = -1;
-
-                for (int i = start; i < end; i++)
-                {
-                    if (this.table[i].HasValue)
-                    {
-                        if (this.table[i].Value.Key.Equals(key))
-                        {
-                            this.table[i] = kvp;
-                            return;
-                        }
-                    }
-                    else if (empty < 0)
-                    {
-                        empty = i;
-                    }
-                }
-
-                if (empty >= 0)
-                {
-                    Debug.Assert(!this.table[empty].HasValue);
-
-                    this.table[empty] = kvp;
-                    this.count++;
-                    Debug.Assert(this.count <= (this.numRows * this.numColumns));
-                    return;
-                }
-
-                // No empty slot, randomly replace.
-                int loc = start + this.rng.Next(this.numColumns);
-                Debug.Assert(this.table[loc].HasValue);
-                Debug.Assert(!this.table[loc].Value.Key.Equals(key));
-                this.table[loc] = kvp;
+                (_, int rowStart, int rowEnd) = this.table.FindRow(key);
+                this.table.Set(key, value, rowStart, rowEnd, this.rng);
             }
         }
-
-        public int Count => this.count;
 
         public void Clear()
         {
-            for (int i = 0; i < this.table.Length; i++)
-            {
-                this.table[i] = null;
-            }
-
-            this.count = 0;
+            this.table.Clear();
         }
 
-        public bool ContainsKey(TKey key) => this.FindEntry(key) >= 0;
+        public bool ContainsKey(TKey key)
+        {
+            (_, int rowStart, int rowEnd) = this.table.FindRow(key);
+            return this.table.FindEntry(key, rowStart, rowEnd) >= 0;
+        }
 
         public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
         {
-            for (int i = 0; i < this.table.Length; i++)
-            {
-                if (table[i].HasValue)
-                {
-                    yield return table[i].Value;
-                }
-            }
+            return this.table.GetEnumerator();
         }
 
-        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => this.table.GetEnumerator();
 
         public bool Remove(TKey key)
         {
-            int loc = this.FindEntry(key);
-            if (loc < 0)
-            {
-                return false;
-            }
-
-            this.table[loc] = null;
-            this.count--;
-            return true;
+            (_, int rowStart, int rowEnd) = this.table.FindRow(key);
+            int loc = this.table.FindEntry(key, rowStart, rowEnd);
+            return this.table.Remove(loc);
         }
 
         public bool TryGetValue(TKey key, out TValue value)
         {
-            int loc = this.FindEntry(key);
-            if (loc >= 0)
-            {
-                KeyValuePair<TKey, TValue>? item = this.table[loc];
-                Debug.Assert(item.HasValue, "item is null");
-                value = item.Value.Value;
-                return true;
-            }
-
-            value = default(TValue);
-            return false;
-        }
-
-        private int FindEntry(TKey key)
-        {
-            (int start, int end) = this.FindRow(key);
-            for (int i = start; i < end; i++)
-            {
-                if (this.table[i].HasValue && this.table[i].Value.Key.Equals(key))
-                {
-                    return i;
-                }
-            }
-
-            return -1;
-        }
-
-        private (int start, int end) FindRow(TKey key)
-        {
-            int hash = key.GetHashCode() & 0x7FFFFFFF;
-            int row = hash % this.numRows;
-            int start = row * this.numColumns;
-            int end = start + this.numColumns;
-            return (start, end);
+            (_, int rowStart, int rowEnd) = this.table.FindRow(key);
+            int loc = this.table.FindEntry(key, rowStart, rowEnd);
+            return this.table.TryGetValue(loc, out value);
         }
     }
 }
---
+++ CacheTable/CacheTableInternal.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace CacheTable
+{
+    struct CacheTableInternal<TKey, TValue>
+    {
+        public readonly KeyValuePair<TKey, TValue>?[] table;
+        public readonly int numRows;
+        public readonly int numColumns;
+        public int count;
+
+        public CacheTableInternal(int numRows, int numColumns)
+        {
+            this.numRows = numRows;
+            this.numColumns = numColumns;
+            this.table = new KeyValuePair<TKey, TValue>?[numRows * numColumns];
+            this.count = 0;
+        }
+
+        public void Clear()
+        {
+            for (int i = 0; i < this.table.Length; i++)
+            {
+                this.table[i] = null;
+            }
+
+            this.count = 0;
+        }
+
+        public (int row, int start, int end) FindRow(TKey key)
+        {
+            int hash = key.GetHashCode() & 0x7FFFFFFF;
+            int row = hash % this.numRows;
+            int start = row * this.numColumns;
+            int end = start + this.numColumns;
+            return (row, start, end);
+        }
+
+        public int FindEntry(TKey key, int rowStart, int rowEnd)
+        {
+            for (int i = rowStart; i < rowEnd; i++)
+            {
+                if (this.table[i].HasValue && this.table[i].Value.Key.Equals(key))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public bool TryGetValue(int loc, out TValue value)
+        {
+            if (loc >= 0)
+            {
+                KeyValuePair<TKey, TValue>? item = table[loc];
+                Debug.Assert(item.HasValue, "item is null");
+                value = item.Value.Value;
+                return true;
+            }
+
+            value = default(TValue);
+            return false;
+        }
+
+        public bool Remove(int loc)
+        {
+            if (loc < 0)
+            {
+                return false;
+            }
+
+            this.table[loc] = null;
+            this.count--;
+            return true;
+        }
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            for (int i = 0; i < this.table.Length; i++)
+            {
+                if (this.table[i].HasValue)
+                {
+                    yield return this.table[i].Value;
+                }
+            }
+        }
+
+        public void Set(TKey key, TValue value, int rowStart, int rowEnd, Random rng)
+        {
+            var kvp = new KeyValuePair<TKey, TValue>(key, value);
+            int empty = -1;
+
+            for (int i = rowStart; i < rowEnd; i++)
+            {
+                if (this.table[i].HasValue)
+                {
+                    if (this.table[i].Value.Key.Equals(key))
+                    {
+                        this.table[i] = kvp;
+                        return;
+                    }
+                }
+                else if (empty < 0)
+                {
+                    empty = i;
+                }
+            }
+
+            if (empty >= 0)
+            {
+                Debug.Assert(!this.table[empty].HasValue);
+
+                this.table[empty] = kvp;
+                this.count++;
+                Debug.Assert(this.count <= (this.numRows * this.numColumns));
+                return;
+            }
+
+            // No empty slot, randomly replace.
+            int loc = rowStart + rng.Next(this.numColumns);
+            Debug.Assert(this.table[loc].HasValue);
+            Debug.Assert(!this.table[loc].Value.Key.Equals(key));
+            this.table[loc] = kvp;
+        }
+    }
+}
