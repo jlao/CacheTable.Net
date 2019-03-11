3a571878470b1e8b263d3ddb5373d3db3d29fd3d
James Lao <git@jameslao.com>
2019-03-10 18:41:32 -0700

    Use fast XorShift RNG.

---
+++ CacheTable/XorShiftRandom.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace CacheTable
+{
+    public class XorShiftRandom
+    {
+        [ThreadStatic]
+        private static readonly Random seedRng = new Random();
+
+        private uint state;
+
+        public XorShiftRandom() : this(GetNonZeroSeed())
+        {
+        }
+
+        public XorShiftRandom(uint seed)
+        {
+            this.state = seed;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public int Next(int max)
+        {
+            uint x = this.state;
+            x ^= x << 13;
+            x ^= x >> 17;
+            x ^= x << 5;
+            this.state = x;
+            return (int)(x % max);
+        }
+
+        private static uint GetNonZeroSeed()
+        {
+            while (true)
+            {
+                uint seed = (uint)seedRng.Next();
+                if (seed != 0)
+                {
+                    return seed;
+                }
+            }
+        }
+    }
+}
